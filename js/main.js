88c2fa869e8680f5a2b642db5fbdc5aa6dc779ef
SaadVSP96 <saadmasood192@gmail.com>
2024-07-28 00:51:08 +0500

    First Draft Complete

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Nav Menu Code:
+
 document.addEventListener("DOMContentLoaded", function () {
   document
     .querySelector(".nav__toggler")
@@ -5,3 +7,38 @@ document.addEventListener("DOMContentLoaded", function () {
       document.querySelector(".nav__list").classList.toggle("hidden");
     });
 });
+
+// Image Carousel Code:
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}

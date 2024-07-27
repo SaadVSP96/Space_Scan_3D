cb2b6de4677f64726c3dee7e8a031637a01e2bab
SaadVSP96 <saadmasood192@gmail.com>
2024-07-28 01:13:27 +0500

    some minor updates

--- js/main.js
+++ js/main.js
@@ -12,21 +12,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
 let slideIndex = 1;
 showSlides(slideIndex);
-
 // Next/previous controls
 function plusSlides(n) {
   showSlides((slideIndex += n));
 }
-
 // Thumbnail image controls
 function currentSlide(n) {
   showSlides((slideIndex = n));
 }
-
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -36,9 +32,5 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
 }
